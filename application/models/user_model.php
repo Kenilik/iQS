28f10633fee8bc6f73cef9cabd27001bb6e0d82b
unknown <shayne@.(none)>
2012-11-14 22:03:27 +1300

    vcsx

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -25,5 +25,53 @@ class User_model extends CI_Model {
 			$q = FALSE; //return false if there is no User by this UserID
 		}
 		return $q;
-	}	
+	}
+	
+	function set_password($user_id, $plaintext)
+	{
+		$this->hashedpassword = $this->hash_password($plaintext);
+		//$this->db->update
+	}
+	
+	function hash_password($password)
+	{
+		$salt = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
+		$hash = hash('sha256', $salt . $password);
+
+		return $salt . $hash;
+	}
+	
+	function validate_password($password)
+	{
+		$salt = substr($this->hashedpassword, 0, 64);
+		$hash = substr($this->hashedpassword, 64, 64);		
+		
+		$password_hash = hash('sha256', $salt . $password);
+		
+		return $password_hash == $hash;
+	}
+	
+	public static function validate_login($username, $password)
+	{
+		$user = User::find_by_username($username);
+		
+		if ($user && $user->validate_password($password)) {
+			User::login($user->id);
+			return $user;
+		} else {			 
+			return FALSE;
+		}
+		
+	}
+	
+	public static function login($username)
+	{
+		$user = User::find_by_username($username);
+		$newdata = array(
+			'iqs_username' => $user->username,
+			'iqs_firstname' => $user->firstname,
+			'iqs_lastname' => $user->firstname);
+			//don't foget to also deal with user role
+		$this->session->set_userdata($newdata);
+	}
 }
